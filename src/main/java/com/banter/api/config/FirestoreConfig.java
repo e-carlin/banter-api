9a291d8d93996b2a60b16455f6ef5ba94a437df6
Evan Carlin <evan@carlin.com>
2018-04-29 13:43:55 -0700

    Remove firebase creds from src

--- src/main/java/com/banter/api/config/FirestoreConfig.java
+++ src/main/java/com/banter/api/config/FirestoreConfig.java
@@ -4,14 +4,17 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseCredential;
 import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class FirestoreConfig {
@@ -20,9 +23,11 @@ public class FirestoreConfig {
 
     public FirestoreConfig() throws IOException {
 
-        FileInputStream firebaseServiceAccount =
-                //TODO: can't use a file name like this. The JSON file should NOT be checked into src control
-                new FileInputStream("banter-firebase-admin.json");
+        String firebaseCreds = System.getenv("FIREBASE_CREDS");
+        InputStream firebaseServiceAccount = new ByteArrayInputStream(firebaseCreds.getBytes(StandardCharsets.UTF_8));
+//        FileInputStream firebaseServiceAccount =
+//                //TODO: can't use a file name like this. The JSON file should NOT be checked into src control
+//                new FileInputStream("banter-firebase-admin.json");
 
         FirebaseOptions firebaseOptions = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(firebaseServiceAccount))
